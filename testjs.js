a8a1420479095ee2968f7ccd6d3bff468972348d
dearam <ramprasath2709@gmail.com>
2023-01-29 22:37:26 +0530

    sent data to another page

--- testjs.js
+++ testjs.js
@@ -1,15 +1,35 @@
 n=document.getElementById("iname");
 m=document.getElementById("iemail");
 d=document.getElementById("idob");
-
+p=document.getElementById("ipassword");
+r=document.getElementsByName("sradio");
+lab=document.getElementsByName("olab");
+check=document.getElementsByName("scheckbox");
+datas=[];
 
 function tosubmition(event){
     event.preventDefault();
     //console.log(event)
     //console.log(event.target.tagName)
-    console.log(d.value);
-    
-    // location.href="summa.html"
+    datas.push(d.value);
+    datas.push(n.value);
+    datas.push(m.value);
+    datas.push(p.value);
+    for(let i=0;i<r.length;i++){
+        if(r[i].checked){
+            console.log(r[i].value);
+        }
+    }
+    let checks=[];
+    for(let i=0;i<check.length;i++){
+        if(check[i].checked){
+            checks.push(check[i].value);
+        }
+    }
+    datas.push(checks);
+    console.log(datas);
+    localStorage.setItem("myvalue",datas);
+    location.href="summa.html"
     // stop bubbling
     //event.stopPropagation();
     // remove all active event listeners for this event
